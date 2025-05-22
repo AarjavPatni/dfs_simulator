a1bb081a41a5ecae42bb2ad7943054948c6f375b
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-22 04:08:47 -0400

    Complete chunker engine

--- src/chunk.rs
+++ src/chunk.rs
@@ -16,36 +16,30 @@ fn main(filepath: String) -> Vec<(ChunkId, Chunk)> {
     let mut reader = BufReader::new(f);
     let mut chunks: Vec<(ChunkId, Chunk)> = vec![];
 
-    loop {
-        let buf = reader.fill_buf().expect("Couldn't create buffer!");
-        let mut offset = 0;
-        let mut eof: bool = false;
+    let mut buf = reader.fill_buf().expect("Couldn't create buffer!");
 
-        while !buf.is_empty() {
-            let file_bytes: Vec<u8> = if buf.len() >= 64 { buf[offset..offset+64].to_vec() } else { buf[offset..].to_vec() };
+    while !buf.is_empty() {
+        let file_bytes: Vec<u8> = if buf.len() >= 64 { buf[..64].to_vec() } else { buf[..].to_vec() };
 
-            if buf.len() < 64 {
-                eof = true;
-            }
+        // TODO: Understand in-depth how hashing really works
+        let chunk_hash = Sha256::digest(&file_bytes);
+        let chunk_hash_str = format!("{:x}", chunk_hash);
 
-            // TODO: Understand in-depth how hashing really works
-            let chunk_hash = Sha256::digest(&file_bytes);
-            let chunk_hash_str = format!("{:x}", chunk_hash);
+        let chunk: Chunk = Chunk {
+            data: file_bytes,
+            id: chunk_hash_str.clone(),
+            metadata: ChunkMetadata,
+        };
 
-            let chunk: Chunk = Chunk {
-                data: file_bytes,
-                id: chunk_hash_str.clone(),
-                metadata: ChunkMetadata,
-            };
+        chunks.push((chunk_hash_str, chunk));
 
-            chunks.push((chunk_hash_str, chunk));
-
-            // TODO: fix this coz the last one may not be 64
-            offset += 64;
+        if buf.len() >= 64 {
             reader.consume(64);
+        } else {
+            reader.consume(buf.len());
         }
 
-        if eof { break; }
+        buf = reader.fill_buf().expect("Couldn't create buffer!");
     }
 
     chunks
