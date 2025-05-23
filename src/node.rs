320f445cdce98bbfe06fa716f4ea7da096554e39
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-23 16:49:37 -0400

    Add Node methods scaffolding

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,4 @@
-use crate::chunk::Chunk;
+use crate::chunk::*;
 
 pub type NodeId = u32;
 
@@ -6,3 +6,12 @@ pub struct Node {
     pub id: NodeId,
     pub chunks: Vec<Chunk>,
 }
+
+pub fn put_chunk(id: ChunkId, data: Chunk) -> NodeId {
+
+}
+
+pub fn get_chunk(id: ChunkId) -> Option<Chunk> {
+
+}
+
