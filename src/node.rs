f51418580738317afb999efbf37e62e37dd8ada9
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-23 18:08:06 -0400

    Move Node methods to impl; add constructor

--- src/node.rs
+++ src/node.rs
@@ -1,17 +1,29 @@
+use std::collections::HashMap;
+
 use crate::chunk::*;
+use crate::catalog::*;
 
 pub type NodeId = u32;
 
 pub struct Node {
     pub id: NodeId,
-    pub chunks: Vec<Chunk>,
+    pub chunks: HashMap<ChunkId, Chunk>,
 }
 
-pub fn put_chunk(id: ChunkId, data: Chunk) -> NodeId {
+impl Node {
+    pub fn new(id: u32) -> Self {
+        Self {
+            id,
+            chunks: HashMap::new(),
+        }
+    }
 
-}
+    pub fn put_chunk(&mut self, data: Chunk) {
+        todo!()
+    }
 
-pub fn get_chunk(id: ChunkId) -> Option<Chunk> {
-
+    pub fn get_chunk(&self, id: &ChunkId) -> Option<&Chunk> {
+        todo!()
+    }
 }
 
